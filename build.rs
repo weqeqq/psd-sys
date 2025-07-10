d5380168a7c891999c36c92831ee04356e8516f6
weqeq <ggmodwear@gmail.com>
2025-07-10 18:30:31 +0300

    .

--- build.rs
+++ build.rs
@@ -48,7 +48,7 @@ fn main() {
     );
     println!("cargo:rustc-link-lib=static=psd");
     println!("cargo:rustc-link-lib=static=file");
-    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() != "windows" {
         println!("cargo:rustc-link-lib=dylib=stdc++");
     }
     println!("cargo:rerun-if-changed=build.rs");
