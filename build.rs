d8d711c9f20b06035a87b80bdffb08733d3817bb
Frederik <147479464+frederik-uni@users.noreply.github.com>
2025-10-08 12:14:54 +0200

    add deflate

--- build.rs
+++ build.rs
@@ -53,5 +53,6 @@ fn main() {
             println!("cargo:rustc-link-lib=dylib=stdc++");
         }
     }
+    println!("cargo:rustc-link-lib=deflate");
     println!("cargo:rerun-if-changed=build.rs");
 }
